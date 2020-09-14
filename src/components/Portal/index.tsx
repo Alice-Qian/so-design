a8971ed7f65d64e7399c068d91a278bed6535338
钱婷婷 <qiantingting@qiantingtingDSC006659.local>
2020-09-14 17:37:28 +0800

    feat: portal

--- src/components/Portal/index.tsx
+++ src/components/Portal/index.tsx
@@ -1,9 +1,2 @@
-import React from "react";
-import ReactDom from "react-dom";
-import { PortalProps } from "./type";
-
-const Portal = ({ node, children }: PortalProps) => {
-  return ReactDom.createPortal(children, node);
-};
-
-export default Portal;
+import Portal from './index'
+export default Portal
--- src/components/Portal/portal.tsx
+++ src/components/Portal/portal.tsx
@@ -1,2 +1,28 @@
-import Portal from './index'
-export default Portal
+import React, { useRef, useEffect } from "react";
+import ReactDom from "react-dom";
+import { PortalProps } from "./type";
+
+const canUseDOM = !!(
+  typeof window != undefined && window.document && window.document.createElement
+)
+
+const Portal = ({ node, children }: PortalProps) => {
+  const defaultNodeRef = useRef<HTMLElement | null>(null);
+  useEffect(() => {
+    if (defaultNodeRef.current) {
+      document.body.removeChild(defaultNodeRef.current);
+    }
+  }, []);
+
+  if(!canUseDOM) return null;
+
+  if (!node && !defaultNodeRef.current) {
+    const defaultNode = document.createElement("div");
+    defaultNode.className = "react-portal";
+    defaultNodeRef.current = defaultNode;
+    document.body.appendChild(defaultNode);
+  }
+  return ReactDom.createPortal(children, (node || defaultNodeRef.current)!); // ! 断言
+};
+
+export default Portal;
